19e152cad16385a1d250ecc412603c0b4a8e1d63
glenluersman <glen.luersman@gmail.com>
2022-02-06 20:40:29 -0500

    Created signup, login, and dashboard handlebars. Added login and logout features.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,6 +3,7 @@ const sequelize = require('../config/connection');
 const { User, Post, Comment } = require('../models');
 
 router.get('/', (req, res) => {
+  console.log(req.session)
   Post.findAll({
     attributes: ['id', 'title', 'post_content', 'created_at'],
     order: [['created_at', 'DESC']],
@@ -24,12 +25,32 @@ router.get('/', (req, res) => {
   .then(dbPostData => {
     const posts = dbPostData.map(post => post.get({ plain: true }));
 
-    res.render('homepage', { posts });
+    res.render('homepage', { 
+      posts, 
+      loggedIn: req.session.loggedIn
+    });
   })
   .catch(err => {
     console.log(err);
     res.status(500).json(err);
   });
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login')
+});
+
+router.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+router.get('/dashboard', (req, res) => {
+  res.render('dashboard');
 });
 
 module.exports = router;
